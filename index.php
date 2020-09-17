1b683eb323847a4ef889642e54f23cafef3ef847
Andrew Mason <andrewmasonmedia@gmail.com>
2020-09-17 15:11:17 -0400

    Initial commit jen mason consulting

---
+++ index.php
@@ -0,0 +1,186 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Jen Mason">
+    <meta name="generator" content="Hugo 0.72.0">
+    <title>Jen Mason Consulting</title>
+
+    <link rel="canonical" href="https://v5.getbootstrap.com/docs/5.0/examples/offcanvas/">
+    <!-- Bootstrap core CSS -->
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="offcanvas.css" rel="stylesheet">
+    <link href="/dist/css/main.css" rel="stylesheet">
+  </head>
+  <body>
+    
+<?php include './includes/menu.php'; ?>
+
+<main class="container-fluid p-0">
+
+  <div class="row m-0 p-0 min-100 bg-rocks">
+    <div class="col-sm-12 align-left-bottom text-light text-shadow p-5 m-0">
+      <!-- <div class="pb-2">
+      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-git-compare" width="60" height="60" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z"/>
+      <circle cx="6" cy="6" r="2" />
+      <circle cx="18" cy="18" r="2" />
+      <path d="M11 6h5a2 2 0 0 1 2 2v8" />
+      <polyline points="14 9 11 6 14 3" />
+      <path d="M13 18h-5a2 2 0 0 1 -2 -2v-8" />
+      <polyline points="10 15 13 18 10 21" />
+    </svg>
+      </div> -->
+      <h1 class="site-title">Jen <span class="d-table">Mason</span> Consulting</h1>
+      <h3 class="py-2">People. Possibility. Purpose.</h3>
+      <div class="inline-block mt-3">
+      <button class="btn btn-light">Learn more</button>
+      <button class="btn btn-dark">Get in touch</button>
+    </div>
+    </div>
+    <div class="offset-lg-6"></div>
+  </div>
+
+  <!-- mission -->
+  <div class="row m-0 p-0">
+    <div class="col-lg-8 offset-lg-4 mx-auto d-flex flex-column justify-content-center align-items-center text-center p-5 mt-5 px-5 pb-2 m-0">
+      <h5 class="text-uppercase font-weight-bold">Mission</h5>
+      <h1 class="font-weight-bold">Enabling groups to 
+        <span class="playfair font-italic text-info">think,</span>
+        <span class="playfair font-italic text-info">learn,</span> and
+        <span class="playfair font-italic text-info">work,</span> together</h1>
+        <p>I am passionate about supporting learning and positive change in groups of all kinds. Blending over 20 years of experience as an educator and process designer with insights from my Ph.D. research about the role of listening in building resilience in complex, adaptive systems, I work with a sense of both purpose and playfulness. My goal is support effective communication and learning, which will enable your group to achieve its goals and thrive.</p>
+    </div>
+  </div><!--end row-->
+
+  <hr class="w-50 mx-auto bg-secondary">
+
+  <!-- icon circles -->
+  <div class="row px-5 pb-5 pt-2">
+    <div class="col-lg-4 p-3 align-center-top">
+      <img src="./assets/img/jm-lego.jpg" class="img-icon-circle img-thumbnail shadow" alt="Group playing lego as team building exercise.">
+      <h5 class="text-uppercase font-weight-bold">Systems Thinking</h5>
+      <p>Healthy systems (groups, organizations, or communities) are those in which there is an open flow of information and ideas.</p>
+    </div>
+    <div class="col-lg-4 p-3 align-center-top">
+      <img src="./assets/img/jm_arms_crossed.png" class="img-icon-circle img-thumbnail shadow" alt="Photo of person with arms crossed representing conflict or blocked communication.">
+      <h5 class="text-uppercase font-weight-bold">Conflict Patterns</h5>
+      <p>That flow can be interrupted, or blocked by conflict, lack of clarity and trust, and habitual egoic listening patterns.</p>
+    </div>
+    <div class="col-lg-4 p-3 align-center-top">
+      <img src="./assets/img/jm_silhouette_group.jpeg" class="img-icon-circle img-thumbnail shadow" alt="Group celebrating together at sunrise.">
+      <h5 class="text-uppercase font-weight-bold">Blockage</h5>
+      <p>When the pathways of communication become blocked, the system cannot function optimally in terms of either repair or growth.</p>
+    </div>
+  </div>
+  <!-- end row -->
+
+  <!-- services -->
+  <div class="row min-100 bg-linen m-0 p-0">
+    <div class="col-lg-8 offset-lg-4 mx-auto d-flex flex-column justify-content-center align-items-center text-center p-5 m-0">
+      <h5 class="text-uppercase font-weight-bold">Services</h5>
+      <h1 class="font-weight-bold">Organizations
+        <span class="playfair font-italic text-info">Thrive</span>
+        when they are able to harness the power of
+        <span class="playfair font-italic text-info">Diverse Ideas</span> and
+        <span class="playfair font-italic text-info">Approaches.</span></h1>
+      <p>Based on my Ph.D research and 20+ years of experience working as an educator, process designer, and facilitator, I offer the following services to meet groups where they are and support them in opening the pathways of communication, enabling the flow of information and ideas, and maximizing their potential.</p>
+    </div>
+  </div><!--end row-->
+  
+  <!-- individual services -->
+
+  <div class="row min-100 m-0 p-0">
+    <div class="col-lg-6 min-50 align-left-center p-5 m-0">
+      <h6 class="text-uppercase small">Address and transform conflicts within your group to get unstuck</h6>
+      <h1 class="section-title">Conflict Transformation</h1> 
+      <p>Transformational team building is a process that enables and empowers groups to work through the issues and conflicts that limit them. Based on the work of master facilitator, Linda Lehtonen, this approach supports “focused, courageous, dialogue that helps the group move to action and resolution and take continuing responsibility for solving their issues on an ongoing basis” (Lehtonen).</p>
+    </div>
+    <div class="col-lg-6 min-50 p-0">
+      <img class="img-full" src="https://images.pexels.com/photos/3183197/pexels-photo-3183197.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+    </div>
+  </div><!--end row-->
+
+  <div class="row min-100 m-0 p-0 bg-linen">
+    <div class="col-lg-6 min-50 p-0">
+      <img class="img-full" src="./assets/img/jm-hand-crystal.jpg" alt="">
+    </div>
+    <div class="col-lg-6 align-left-center p-5 m-0">
+      <h6 class="text-uppercase small">Develop capacity to engage with people who think differently than you do</h6>
+      <h1 class="section-title">Listening & Perspective-Taking Workshops</h1> 
+      <p>Using skills developed in the Listening and Perspective-Taking Workshops, groups can participate in facilitated practice dialogues on a range of topics to solidify their skills.</p>
+    </div>
+  </div><!--end row-->
+
+  <!-- service 3 -->
+  <div class="row min-100 m-0 p-0">
+    <div class="col-lg-6 min-50 align-left-center p-5 m-0">
+      <h6 class="text-uppercase small">Practice your listening and perspective-taking skills in a facilitated setting</h6>
+      <h1 class="section-title">Facilitated Practice Dialogues</h1> 
+      <p>Transformational team building is a process that enables and empowers groups to work through the issues and conflicts that limit them. Based on the work of master facilitator, Linda Lehtonen, this approach supports “focused, courageous, dialogue that helps the group move to action and resolution and take continuing responsibility for solving their issues on an ongoing basis” (Lehtonen).</p>
+    </div>
+    <div class="col-lg-6 min-50 p-0">
+      <img class="img-full" src="./assets/img/jm_group_dialogue.png" alt="">
+    </div>
+  </div><!--end row-->
+
+  <div class="row min-100 m-0 p-0 bg-linen">
+    <div class="col-lg-6 min-50 p-0">
+      <img class="img-full" src="./assets/img/decision_making.jpeg" alt="">
+    </div>
+    <div class="col-lg-6 align-left-center p-5 m-0">
+      <h6 class="text-uppercase small">Engage, explore, decide, create</h6>
+      <h1 class="section-title">Group Facilitation</h1> 
+      <p>I can help your team engage with one another productively, explore ideas, and make decisions using a variety of methodologies including Bohmian dialogue, Future Search, The Way of Council, World Café, Open Space, Focused Conversations, and others.</p>
+    </div>
+  </div><!--end row-->
+  
+  <div class="row min-100 m-0 p-0">
+    <div class="col-lg-6 min-50 align-left-center p-5 m-0">
+      <h6 class="text-uppercase small">Connect with your team in a restorative natural setting</h6>
+      <h1 class="section-title">Team Development Retreats in Natural Settings</h1> 
+      <p>Drawing on my experience as an outdoor, experiential, and adventure educator, I can design one or multi-day retreats to meet the needs of your group. Away from the demands of the day-to-day, and located in beautiful, natural settings, retreats can incorporate and blend any of the services I offer and include team building and nature connection experiences to help your group ground/connect/transform.</p>
+    </div>
+    <div class="col-lg-6 min-50 p-0">
+      <img class="img-full" src="./assets/img/jm-campfire2.jpg" alt="Image of campfire with starry sky representing team development in outdoor retreat settings.">
+    </div>
+  </div><!--end row-->
+
+
+  
+  <!-- about -->
+  <div class="row min-100 bg-linen m-0 p-0">
+    <div class="col-lg-8 offset-lg-4 mx-auto d-flex flex-column justify-content-center align-items-center text-center p-5 m-0">
+      <h5 class="text-uppercase font-weight-bold">About Jen</h5>
+      <h1 class="font-weight-bold">Enabling groups to 
+        <span class="playfair font-italic text-info">Think,</span>
+        <span class="playfair font-italic text-info">Learn,</span> and
+        <span class="playfair font-italic text-info">Work,</span> together</h1>
+      <p>I have a Ph.D. in Sustainability Education and a Master’s degree in Education from
+Prescott College, and I have a Bachelor of Education degree with a focus in Outdoor
+and Experiential Education and a Bachelor of Arts degree in Political Science from
+Queen’s University.
+My work and research sit at the confluence of the following: sustainability education,
+resilience and complexity theory, multi-stakeholder whole-system change processes,
+dialogue theory and practice, organizational learning, mindfulness, and transformative
+learning theory.
+Bringing my love of learning and deep humility, I work with a sense of thoughtful
+purpose and infectious playfulness. I’d love to connect with you to find out how I can
+support you and your team/organization/community.</p>
+    </div>
+  </div><!--end row-->
+
+
+
+  </div><!-- end container-->
+</main>
+
+
+    <script src="/assets/js/bootstrap.bundle.min.js"></script>
+
+      <script src="offcanvas.js"></script>
+  </body>
+</html>
