2080b0a9a4556a57c9267d4cfc409dc64d7a1313
Andrew Mason <andrewmasonmedia@gmail.com>
2020-09-27 15:56:33 -0400

    Added smooth scroll library, updated banner title color, edited mission text, added bulb image

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// Scroll to specific values
+// scrollTo is the same
+// window.scroll({
+//     top: 2500, 
+//     left: 0, 
+//     behavior: 'smooth'
+//   });
+
+  var scroll = new SmoothScroll('a[href*="#"]',{
+    easing: 'easeInQuad',
+    speed: 300
+});
