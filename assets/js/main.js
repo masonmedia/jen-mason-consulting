8bd813597620cbf6edd36776177398b51625874f
Andrew Mason <andrewmasonmedia@gmail.com>
2020-09-30 22:34:31 -0400

    Attempt installing Bootstrap 4 CDN to fix animation issue in Safari

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,19 +10,6 @@ sal();
 
 AOS.init();
 
-$('.aniview').AniView();
-
-// window.onload = function() {
-// 	lax.setup() 
-
-// 	const updateLax = () => {
-// 		lax.update(window.scrollY)
-// 		window.requestAnimationFrame(updateLax)
-// 	}
-
-// 	window.requestAnimationFrame(updateLax)
-// }
-
   var scroll = new SmoothScroll('a[href*="#"]',{
     easing: 'easeInOutQuad',
     speed: 500
@@ -32,22 +19,6 @@ const menuItem = document.querySelector('.nav-link');
 menuItem.addEventListener('click', function() {
   document.querySelector('.navbar-collapse').classList.remove('show');
 })
-
-// close mobile menu on link click
-  // function myFunction(x) {
-  //     if (x.matches) { 
-  //         let nav = document.querySelectorAll(".nav-item");
-  //         let i;
-  //         for (i = 0; i < nav.length; i++) {
-  //             nav[i].setAttribute("data-toggle", "collapse");
-  //             nav[i].setAttribute("data-target", ".navbar-collapse");
-  //         }
-  //     }
-  //   }
-    
-  //   var x = window.matchMedia("(max-width: 992px)")
-  //   myFunction(x) 
-  //   x.addListener(myFunction) 
 
 // copyright get current year
   const d = new Date();
